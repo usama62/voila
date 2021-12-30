e8094faebee7b95f76597b6710f893308c1d573e
usama.ebrahim <usama.ebrahim@sitex.digital>
2021-12-30 17:55:24 +0500

    categories screen finishing

--- lib/categories.dart
+++ lib/categories.dart
@@ -16,96 +16,346 @@ class _categoriesState extends State<categories> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: const Color(0xFFFFFFFF),
-        body: Column(children: [
-          Container(
-              height: 200,
-              decoration: const BoxDecoration(
-                color: Color(0xFF36BDA4),
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(0.0),
-                  topRight: Radius.circular(0.0),
-                  bottomLeft: Radius.circular(50.0),
-                  bottomRight: Radius.circular(50.0),
+        body: SingleChildScrollView(
+          child: Column(children: [
+            Container(
+                height: 200,
+                decoration: const BoxDecoration(
+                  color: Color(0xFF36BDA4),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(0.0),
+                    topRight: Radius.circular(0.0),
+                    bottomLeft: Radius.circular(50.0),
+                    bottomRight: Radius.circular(50.0),
+                  ),
                 ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20.0, 40.0, 20.0, 10.0),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            RichText(
+                              text: const TextSpan(
+                                text: 'Hi John,\n',
+                                style: TextStyle(
+                                    color: Color(0xFFFFFFFF),
+                                    fontFamily: "PoppinsSemiBold",
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w600),
+                                children: <TextSpan>[
+                                  TextSpan(
+                                    text: 'Need some help today?',
+                                    style: TextStyle(
+                                        color: Color(0xFFFFFFFF),
+                                        fontFamily: "PoppinsSemiBold",
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w400),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Image.asset('assets/images/p_img_small.png')
+                          ]),
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: const Color(0xFFFFFFFF),
+                            hintText: "Find it here.....",
+                            hintStyle: const TextStyle(
+                                color: Color(0xFF373737),
+                                fontWeight: FontWeight.w400,
+                                fontFamily: "PoppinsSemiBold",
+                                fontSize: 16),
+                            prefixIcon: const Icon(Icons.search),
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                    color: Color(0xFFFFFFFF), width: 1.0),
+                                borderRadius: BorderRadius.circular(15.0)),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                    color: Color(0xFFFFFFFF), width: 1.0),
+                                borderRadius: BorderRadius.circular(15.0)),
+                            contentPadding: const EdgeInsets.symmetric(
+                                vertical: 5.0, horizontal: 10.0),
+                          ),
+                        ))
+                  ],
+                )),
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                child: Text("Categories",
+                    style: TextStyle(
+                        color: Color(0xFF36BDA4),
+                        fontWeight: FontWeight.w600,
+                        fontFamily: "PoppinsSemiBold",
+                        fontSize: 18)),
               ),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(20.0, 40.0, 20.0, 10.0),
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 10.0),
+                    child: Container(
+                      height: 78,
+                      decoration: BoxDecoration(
+                          image: const DecorationImage(
+                            image: AssetImage('assets/images/CLEANING.png'),
+                            fit: BoxFit.cover
+                          ),
+                          borderRadius: BorderRadius.circular(15.0)
+                        ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          RichText(
-                            text: const TextSpan(
-                              text: 'Hi John,\n',
-                              style: TextStyle(
-                                  color: Color(0xFFFFFFFF),
-                                  fontFamily: "PoppinsSemiBold",
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.w600),
-                              children: <TextSpan>[
-                                TextSpan(
-                                  text: 'Need some help today?',
-                                  style: TextStyle(
-                                      color: Color(0xFFFFFFFF),
-                                      fontFamily: "PoppinsSemiBold",
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w400),
-                                ),
-                              ],
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(8.0,0.0,0.0,0.0),
+                            child: RichText(
+                              text: const TextSpan(
+                                text: 'CLEANING',
+                                style: TextStyle(
+                                    color: Color(0xFFFFFFFF),
+                                    fontFamily: "PoppinsSemiBold",
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w600),
+                                children: [
+                                  WidgetSpan(
+                                    child: Padding(
+                                      padding: EdgeInsets.fromLTRB(5.0,0.0,0.0,0.0),
+                                      child: Icon(
+                                        Icons.east,
+                                        color: Color(0xFFFFFFFF)
+                                      ),
+                                    )
+                                  )
+                                ],
+                              ),
                             ),
                           ),
-                          Image.asset('assets/images/p_img_small.png')
-                        ]),
+                        ],
+                      ),
+                    ),
                   ),
-                  Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: const Color(0xFFFFFFFF),
-                          hintText: "Find it here.....",
-                          hintStyle: const TextStyle(
-                              color: Color(0xFF373737),
-                              fontWeight: FontWeight.w400,
-                              fontFamily: "PoppinsSemiBold",
-                              fontSize: 16),
-                          prefixIcon: const Icon(Icons.search),
-                          focusedBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                  color: Color(0xFFFFFFFF), width: 1.0),
-                              borderRadius: BorderRadius.circular(15.0)),
-                          enabledBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                  color: Color(0xFFFFFFFF), width: 1.0),
-                              borderRadius: BorderRadius.circular(15.0)),
-                          contentPadding: const EdgeInsets.symmetric(
-                              vertical: 5.0, horizontal: 10.0),
-                        ),
-                      ))
-                ],
-              )),
-          const Align(
-            alignment: Alignment.centerLeft,
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-              child: Text("Categories",
-                  style: TextStyle(
-                      color: Color(0xFF36BDA4),
-                      fontWeight: FontWeight.w600,
-                      fontFamily: "PoppinsSemiBold",
-                      fontSize: 18)),
+                ),
+              ],
             ),
-          ),
-          ListBody(
-            children: [
-              Container(
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('assets/images/CLEANING.png'))),
-              )
-            ],
-          )
-        ]));
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 10.0),
+                    child: Container(
+                      height: 78,
+                      decoration: BoxDecoration(
+                        image: const DecorationImage(
+                          image: AssetImage('assets/images/FURNITUREASSEMBLY.png'),
+                          fit: BoxFit.cover
+                        ),
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(8.0,0.0,0.0,0.0),
+                            child: Text("FURNITURE ASSEMBLY",
+                              style: TextStyle(
+                              color: Color(0xFFFFFFFF),
+                              fontFamily: "PoppinsSemiBold",
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600
+                            ),
+                          ),                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 10.0),
+                    child: Container(
+                      height: 78,
+                      decoration: BoxDecoration(
+                        image: const DecorationImage(
+                          image: AssetImage('assets/images/PLUMBING.png'),
+                          fit: BoxFit.cover
+                        ),
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(8.0,0.0,0.0,0.0),
+                            child: Text("PLUMBING",
+                              style: TextStyle(
+                              color: Color(0xFFFFFFFF),
+                              fontFamily: "PoppinsSemiBold",
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600
+                            ),
+                          ),                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 10.0),
+                    child: Container(
+                      height: 78,
+                      decoration: BoxDecoration(
+                        image: const DecorationImage(
+                          image: AssetImage('assets/images/ELECTRICAL.png'),
+                          fit: BoxFit.cover
+                        ),
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(8.0,0.0,0.0,0.0),
+                            child: Text("ELECTRICAL",
+                              style: TextStyle(
+                              color: Color(0xFFFFFFFF),
+                              fontFamily: "PoppinsSemiBold",
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600
+                            ),
+                          ),                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 10.0),
+                    child: Container(
+                      height: 78,
+                      decoration: BoxDecoration(
+                        image: const DecorationImage(
+                          image: AssetImage('assets/images/PAINTING.png'),
+                          fit: BoxFit.cover
+                        ),
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(8.0,0.0,0.0,0.0),
+                            child: Text("PAINTING",
+                              style: TextStyle(
+                              color: Color(0xFFFFFFFF),
+                              fontFamily: "PoppinsSemiBold",
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600
+                            ),
+                          ),                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 10.0),
+                    child: Container(
+                      height: 78,
+                      decoration: BoxDecoration(
+                        image: const DecorationImage(
+                          image: AssetImage('assets/images/PAINTING.png'),
+                          fit: BoxFit.cover
+                        ),
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(8.0,0.0,0.0,0.0),
+                            child: Text("GENERAL HANDYMAN",
+                              style: TextStyle(
+                              color: Color(0xFFFFFFFF),
+                              fontFamily: "PoppinsSemiBold",
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600
+                            ),
+                          ),                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ]),
+        ),
+        bottomNavigationBar: BottomNavigationBar(  
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: const Color(0xFFEE8823),
+          backgroundColor: Colors.white  ,
+          unselectedItemColor: const Color(0xFF373737),
+          items: const <BottomNavigationBarItem>[  
+            BottomNavigationBarItem(  
+              icon: ImageIcon(
+                AssetImage('assets/images/home_icon.png'),
+              ),  
+              label: 'Home',  
+            ),  
+            BottomNavigationBarItem(  
+              icon: ImageIcon(
+                AssetImage('assets/images/booking_icon.png'),
+              ),  
+              label: 'Bookings',  
+            ), 
+            BottomNavigationBarItem(  
+              icon:  ImageIcon(
+                AssetImage('assets/images/chat_icon.png'),
+              ), 
+              label: 'Chat',  
+            ),  
+            BottomNavigationBarItem(  
+              icon: ImageIcon(
+                AssetImage('assets/images/notification_icon.png'),
+              ),
+              label: 'Notifications',  
+            ),  
+          ]
+        )
+      );
   }
 }
